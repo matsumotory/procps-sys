89740f055338ab4b663e836f085384c5502ed560
MATSUMOTO Ryosuke <matsu1229@gmail.com>
2017-05-11 09:32:34 +0000

    Enable PROC_FILLCOM

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate procps_sys;
 
-use procps_sys::readproc::{openproc, readproc, closeproc, proc_t, freeproc};
+use procps_sys::readproc::{openproc, readproc, closeproc, proc_t};
 use std::ffi::CStr;
 use std::ptr::null_mut;
 
@@ -8,8 +8,8 @@ use std::ptr::null_mut;
 fn main() {
     unsafe {
         // intialize query for process list
-        let proctab = openproc(/* fills cmdline line attribute */
-                               procps_sys::readproc::PROC_FILLSTAT | procps_sys::readproc::PROC_FILLSTATUS);
+        let proctab = openproc(procps_sys::readproc::PROC_FILLSTAT | procps_sys::readproc::PROC_FILLSTATUS |
+                               procps_sys::readproc::PROC_FILLCOM);
 
         // go through all processes
         let mut procinfo = proc_t::default();
