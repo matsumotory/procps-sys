b64057be78c7c4dbfee0821550cda3410c0221fb
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2018-11-21 16:09:31 +0900

    Support development environment using vagrant

---
+++ misc/provision.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+
+sudo apt-get update
+sudo apt-get -y install build-essential rake bison git gperf automake m4 \
+                autoconf libtool cmake pkg-config libcunit1-dev ragel \
+                libprocps4-dev jq
+sudo apt-get -y remove nano
+
+curl https://sh.rustup.rs -sSf > rustup.sh
+sh rustup.sh -y
+source $HOME/.cargo/env
+rustup update
+rustup install nightly
+rustup component add rustfmt-preview --toolchain nightly
+rustup default nightly
+
+git clone https://github.com/matsumotory/procps-sys
+cd procps-sys && cargo build
